f81bed25ed87f0963e5c713bb5ad314cfa2115c0
pjltest <2459160418@qq.com>
2019-04-24 10:48:16 +0800

    debug ntc temperature sensor

--- src/app/ntc_temperature_sensor.c
+++ src/app/ntc_temperature_sensor.c
@@ -6,28 +6,23 @@
 #include "gpio.h"
 #include <math.h>
 #include <stdio.h>
-#include <string.H>
+#include <string.h>
 
 /*************	本地常量声明	**************/
 const float	T2  = 273.15 + 25.0;
-const float	RT2	= 10000;	//NTC电阻25℃时阻值
+const u16	RT	= 10000;	//NTC电阻25℃时阻值
 const u16	Bx  = 3950;		//B值
 const float Ka  = 273.15;	//开尔文
 const float Rup = 20000;	//上拉电阻	
 /*************	本地变量声明	**************/
 static u16 Nref = 0;	//基准电压对应AD值
 static char buf[12] = {0};
-const  int	vcc = 5000 ;			//上拉电压
+static  int	vcc = 5000 ;			//上拉电压
 
 /*************	本地函数声明	**************/
-static void	ADC0_Init(void);
-static void	LED_Init(void);
-void Ntc_Tprt_Sensor_Init(void);
+
+
 /*************  外部函数和变量声明 *****************/
-
-
-
-
 
 
 static void	ADC0_Init(void)
@@ -46,27 +41,31 @@ static void	ADC0_Init(void)
 static void	LED_Init(void)
 {
 	GPIO_InitTypeDef	GPIO_InitStructure;		//结构定义
-	GPIO_InitStructure.Pin  = GPIO_Pin_5;		//指定要初始化的IO, GPIO_Pin_0 ~ GPIO_Pin_7, 或操作
+	GPIO_InitStructure.Pin  = GPIO_Pin_5 ;		//指定要初始化的IO, GPIO_Pin_0 ~ GPIO_Pin_7, 或操作
 	GPIO_InitStructure.Mode = GPIO_OUT_PP;		//指定IO的输入或输出方式,GPIO_PullUp,GPIO_HighZ,GPIO_OUT_OD,GPIO_OUT_PP
 	GPIO_Inilize(GPIO_P3,&GPIO_InitStructure);	//初始化
 	
 }
 
 
-
+/*------------------------------------
+-	function:初始化LED、ADC0、LCD
+-			 获取电压基准值	 
+--------------------------------------*/
 void Ntc_Tprt_Sensor_Init(void)
 {
 	LED_Init();
 	ADC0_Init();
 	Lcd_Init();
-	
+		
+
 	GUI_Clear(White);
 
 	GUI_sprintf_hzstr16x(10,10, "热敏传感器测试", Black, White);
 	GUI_sprintf_hzstr16x(10,30, "基准电压:", Black, White );
-	GUI_sprintf_hzstr16x(10,50, "AD电压:", Black, White );
-	GUI_sprintf_hzstr16x(10,70, "NTC阻值:  ", Black, White );
-	GUI_sprintf_hzstr16x(10,90, "温度值:  ", Black, White );
+	GUI_sprintf_hzstr16x(10,70, "AD电压:", Black, White );
+	GUI_sprintf_hzstr16x(10,90, "NTC阻值:  ", Black, White );
+	GUI_sprintf_hzstr16x(10,110, "温度值:  ", Black, White );
 
 	//读内部基准电压, P1ASF=0, 读0通道,实际读9通道
 	//Vref = Nref * Vcc / 1024   Vref = 1250mv
@@ -77,13 +76,15 @@ void Ntc_Tprt_Sensor_Init(void)
 	GUI_sprintf_hzstr16x(90,30,buf,Red,White);
 	
 	//通过基准电压计算出输入电压
-	//vcc = ((float)1250 / Nref * 1024); 	
+//	vcc = (u32)1250 * (u32)vcc / Nref ;
+	sprintf(buf, "vcc： %d", vcc);	 	
+	GUI_sprintf_hzstr16x(10,50,buf,Red,White);
 }
 
 
 /*------------------------------------
 B=ln(RT1/ RT2)/(1/T1-1/T2)
-1/T1 = ln(RT1/ RT2)	/ B + 1/T2
+T1 = 1/(ln(RT1/ RT2)/B + 1/T2) - 273.15 + 0.5
 --------------------------------------*/
 void  Ntc_Tprt_Sensor_GetTptr(void)
 {
@@ -94,25 +95,25 @@ void  Ntc_Tprt_Sensor_GetTptr(void)
 	while(1){
 		delay_ms(500);
 		P35 = !P35;
-		
+			
 		//读ADC0电压
 		//Ux = Vref * Nx / Nref. Vref = 1250mV
 		tptr_value =  Get_ADC10bitResult(0);
 		tptr_value = (u32)1250 * (u32)tptr_value / Nref ;
 		sprintf(buf, "%.3f %c",(float)tptr_value/1000, 'V' );
-		GUI_sprintf_hzstr16x(90,50,buf,Red,White);
+		GUI_sprintf_hzstr16x(90,70,buf,Red,White);
 
 		//计算出NTC阻值
 		RT1 = (float)tptr_value / ((float)(vcc - tptr_value)/Rup);
 		sprintf(buf, "%.3fR", RT1);		
-		GUI_sprintf_hzstr16x(90,70,buf,Red,White);
-
+		GUI_sprintf_hzstr16x(90,90,buf,Red,White);
+		
 		//根据RT1计算对应温度T1
-		T1=((float)1.0/(log(RT1/ RT2)/Bx + (float)1.0/T2) - Ka + 0.5);
-		memset(buf,0,12);
-		sprintf(buf, "%.3f %s",T1, "℃" );
-		GUI_sprintf_hzstr16x(90,90,buf,Red,White); 		
+		T1=((float)1.0/(log(RT1/10000)/Bx + (float)1.0/T2) - Ka + 0.5);
 
+		memset(buf,0,12);
+		sprintf(buf, "%.3f %s %d",T1, "℃" ,RT);
+		GUI_sprintf_hzstr16x(90,110,buf,Red,White); 		
 
 	}
 }
